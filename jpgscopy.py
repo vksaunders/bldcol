06e5bff27b8170b062f21baaefd33334bdfecf8b
vksaunders <vksaunders@gmail.com>
2018-12-12 09:57:44 -0500

    Update jpgscopy.py

--- jpgscopy.py
+++ jpgscopy.py
@@ -3,11 +3,23 @@ import shutil
 import csv
 
 
-dir_src = ("\\Users\\Valerie\\Documents\\GitHub\\bldcol\\Sourcejpg")
-dir_dst = ("\\Users\\Valerie\\Documents\\GitHub\\bldcol\\Destjpg")
+with open ('filenames.BLmountainpics.csv','r') as f:
+    reader = csv.reader(f)
+    next(reader, None)
 
+    for row in reader:
+         #print(row)
 
-for f in os.listdir(dir_src):
-    src_file = os.path.join(dir_src, f)
-    dst_file = os.path.join(dir_dst, f)
-    shutil.copy(src_file, dst_file)
+        valid_files = row[0]+'.jpg'
+
+        full_name_src = "\\Users\\Valerie\\Documents\\GitHub\\bldcol\\Sourcejpg" + valid_files
+        full_name_dest = "\\Users\\Valerie\\Documents\\GitHub\\bldcol\\Destjpg" + valid_files
+
+        try:
+            shutil.copy(full_name_src, full_name_dest)
+
+         #print(valid_files)
+
+        except FileNotFoundError as fnf_error:
+            print(fnf_error)
+            continue
