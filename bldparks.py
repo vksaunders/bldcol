2ea7ca06f3522c856f87888184ef9c3c28d75aa7
vksaunders <vksaunders@gmail.com>
2018-12-12 10:36:59 -0500

    Update bldparks.py

--- bldparks.py
+++ bldparks.py
@@ -18,9 +18,9 @@ with open ('RF_Contributor_Metadata.csv','r') as f:
 
 
 #Use this to write all rows of metadata for selected condition
-        # if 'tourist attraction' in str(keyword1):
+        # if 'park' in str(keyword1):
         #
-        #     tourismpics.append(tourismpics)
+        #     parkspics.append(tourismpics)
 
     print(len(parkspics))
     #print(parkspics)
